1cd735c3e482eb18a7995cbccf54dcdcb6dcb6d3
sens1tiv <25586230+sens1tiv@users.noreply.github.com>
2019-11-11 19:15:22 +0100

    Added well working Database class

---
+++ Snowbird/Final Test/Database.cs
@@ -0,0 +1,175 @@
+﻿// Add MySql Library
+using MySql.Data.MySqlClient;
+
+using System;
+using System.Collections.Generic;
+
+namespace Final_Test {
+    class Database {
+
+        private MySqlConnection connection;
+        private string server, database, uid, password;
+
+        /// <summary>
+        /// Create an instance to a database
+        /// </summary>
+        /// <param name="server">The server IP Address</param>
+        /// <param name="database">The database name</param>
+        /// <param name="uid">MySQL server username</param>
+        /// <param name="password">MySQL server password</param>
+        public Database(string server, string database, string uid, string password) {
+            Initialize(server, database, uid, password);
+        }
+
+        // Initialize values
+        private void Initialize(string server, string database, string uid, string password) {
+            this.server = server;
+            this.database = database;
+            this.uid = uid;
+            this.password = password;
+            
+            string connectionString  = "SERVER=" + server + ";DATABASE=" + database + ";UID=" + uid + ";PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+        }
+
+
+        // Open connection to database
+        private bool OpenConnection() {
+            try {
+                connection.Open();
+                return true;
+            } catch (MySqlException ex) {
+
+                switch (ex.Number) {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server. Contact administrator!");
+                        break;
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again!");
+                        break;
+                    default:
+                        Console.WriteLine("We have no idea what happened. Please stay tuned! p.s.: FIX THIS DAMNED PROGRAM!!!");
+                        break;
+                }
+
+                return false;
+            }
+        }
+
+        // Close connection
+        private bool CloseConnection() {
+            try {
+                connection.Close();
+                return true;
+            } catch (MySqlException ex) {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+        
+
+        /// <summary>
+        /// Insert, Update, Delete statements. Doesn't return any values
+        /// </summary>
+        /// <param name="query">MySQL Command</param>
+        public void NonQuery(string query) {
+
+            // Open connection
+            if (this.OpenConnection() == true) {
+                try {
+
+                    // Create command and assign the query and connection from the constructor
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                    // Execute command
+                    cmd.ExecuteNonQuery();
+
+                } catch (MySqlException ex) {
+                    Console.WriteLine(ex.Message);
+                }
+
+                // Close connection
+                this.CloseConnection();
+            }
+
+        }
+
+        /// <summary>
+        /// MySQL Select statement
+        /// </summary>
+        /// <param name="query">MySQL Command</param>
+        /// <param name="columns">How many columns to ask for</param>
+        /// <param name="columnsName">The names of the asked columns</param>
+        /// <returns>A string type List of the returned entries</returns>
+        public List<string>[] Select(string query, int columns, string[] columnsName) {
+
+            // A list to store the results
+            List<string>[] records = new List<string>[columns];
+            for (int i = 0; i < columns; i++) {
+                records[i] = new List<string>();
+            }
+
+            // Open connection
+            if (this.OpenConnection()) {
+                try {
+
+                    // Create command
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    // Create a data reader amd Execute the command
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                    // Read the data and sotre them in the ArrayList
+                    if (dataReader.HasRows) {
+                        while (dataReader.Read()) {
+                            for (int i = 0; i < columns; i++) {
+                                records[i].Add(dataReader[columnsName[i]] + "");
+                            }
+                        }
+                    }
+
+                    // Close Data Reader
+                    dataReader.Close();
+
+                } catch (MySqlException ex) {
+                    Console.WriteLine(ex.Message);
+                }
+
+                // Close Connection
+                this.CloseConnection();
+            }
+
+            // Return list to be displayed
+            return records;
+        }
+        
+        /// <summary>
+        /// MySQL Count statement
+        /// </summary>
+        /// <param name="query">MySQL Command</param>
+        /// <returns>The number of entries present in the database</returns>
+        public int Count(string query) {
+
+            int Count = -1;
+
+            // Open connection
+            if (this.OpenConnection()) {
+                try {
+                    // Create MySql command
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                    // ExecuteScalar will return one value
+                    Count = int.Parse(cmd.ExecuteScalar() + "");
+
+                } catch (MySqlException ex) {
+                    Console.WriteLine(ex.Message);
+                }
+
+                // Close connection
+                this.CloseConnection();
+            }
+
+            return Count;
+        }
+        
+    }
+}
--- Snowbird/Final Test/Snowbird.cs
+++ Snowbird/Final Test/Snowbird.cs
@@ -1,18 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Final_Test {
     class Snowbird {
-        static void Main(string[] args) {
 
+        static Database db;
+
+        static void Main(string[] args) {
+            db = new Database("127.0.0.1", "snowbird", "root", "");
 
 
         }
 
 
-        // public static void getHashedPass()
+        /// <summary>
+        /// Safely gets and returns the hashed password as a string
+        /// </summary>
+        /// <returns>The SHA256 hashed password</returns>
+        public static string getHashedPass() {
+            string passwd = "";
+            ConsoleKeyInfo key;
+
+            do {
+                key = Console.ReadKey(true);
+
+                // Ignore any key out of range
+                if (((int)key.Key) >= 32 && ((int)key.Key) <= 126) {
+                    // Append the character to the password
+                    passwd += key.KeyChar;
+                    Console.Write("*");
+                }
+                // Exit if Enter key is pressed
+            } while (key.Key != ConsoleKey.Enter);
+            Console.WriteLine();
+
+            return ComputeSha256Hash(passwd);
+        }
+
+        /// <summary>
+        /// Computes the SHA256 hash of a given string
+        /// </summary>
+        /// <param name="rawData">The string you want to get the has of</param>
+        /// <returns>The hashed string of the given input</returns>
+        public static string ComputeSha256Hash(string rawData) {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create()) {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++) {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
     }
 }
