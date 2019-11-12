6c4cedccda42fba4266fa5eb5c58270250d54181
sens1tiv <25586230+sens1tiv@users.noreply.github.com>
2019-11-12 20:02:05 +0100

    Added some basic stuff

--- Snowbird/Final Test/Database.cs
+++ Snowbird/Final Test/Database.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 
 namespace Final_Test {
-    class Database {
+    public class Database {
 
         private MySqlConnection connection;
         private string server, database, uid, password;
---
+++ Snowbird/Final Test/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Test {
+    public class User {
+
+        private string username, userid;
+
+        public User(string username, string userid) {
+            this.username = username;
+            this.userid = userid;
+        }
+
+        public string Username {
+            get { return username; }
+        }
+        public string Userid {
+            get { return userid; }
+        }
+
+    }
+}
