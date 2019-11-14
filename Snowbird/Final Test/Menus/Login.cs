bfad73c7f895f46e8f4572f935b43ba1846b93d1
Ádám Patrik <56996423+CeelNok@users.noreply.github.com>
2019-11-14 10:00:48 +0100

    insert into wallets

--- Snowbird/Final Test/Menus/Login.cs
+++ Snowbird/Final Test/Menus/Login.cs
@@ -36,6 +36,7 @@ namespace Final_Test.Menus {
                             
                             Snowbird.user = new User(identifier, getUserId(identifier));
                             Snowbird.user.WalletCount = Snowbird.db.Count("SELECT COUNT(*) FROM wallets WHERE user_id='" + Snowbird.user.UserId + "';");
+                            if (Snowbird.user.WalletCount == 0) createFirstWallet(Snowbird.user.UserId);
                             Snowbird.user.Wallets = getWallets(Snowbird.user.UserId);
                             break;
 
@@ -52,6 +53,39 @@ namespace Final_Test.Menus {
 
         public static List<string>[] getWallets(string userId) {
             return Snowbird.db.Select("SELECT * FROM wallets WHERE user_id='" + userId + "' ORDER BY type;", 9, new string[9] { "id", "user_id", "type", "amount", "currency", "account_name", "account_number", "description", "created_at" });
+        }
+
+        public static void createFirstWallet(string userId)
+        {
+            Console.Clear();
+            Console.WriteLine("Create your first wallets\n");
+            int amount = 0;
+            string currency = "HUF";
+            //amount
+            Console.Write("Amount: ");
+            amount = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Currency (HUF - 1 / EUR - 2 / USD - 3): ");
+            switch(Snowbird.keyPressed())
+            {
+                case "1":
+                    currency = "huf";
+                    break;
+                case "2":
+                    currency = "eur";
+                    break;
+                case "3":
+                    currency = "usd";
+                    break;
+                default: break;
+            }
+
+            DateTime myDateTime = DateTime.Now;
+            string dateTime = myDateTime.ToString("yyyy-MM-dd HH:mm:ss");
+
+            string query = "INSERT INTO `wallets` (`id`, `user_id`, `type`, `amount`, `currency`, `account_name`, `account_number`, `description`, `created_at`) VALUES (NULL, '" + userId + "', '0', '" + amount + "', '" + currency + "', NULL, NULL, NULL, '" + dateTime + "');";
+            
+            Snowbird.db.NonQuery(query);
         }
 
     }
