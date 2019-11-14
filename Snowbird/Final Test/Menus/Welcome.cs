7e98026d7b1dc297974f8245e2f8f94b72f08051
Ádám Patrik <56996423+CeelNok@users.noreply.github.com>
2019-11-14 08:14:34 +0100

    Welcome menu

--- Snowbird/Final Test/Menus/Welcome.cs
+++ Snowbird/Final Test/Menus/Welcome.cs
@@ -4,8 +4,25 @@ namespace Final_Test.Menus {
     public static class Welcome {
 
         public static void Run() {
+            Console.WriteLine("### Házi Pénztár ###\n");
+            Console.WriteLine("1. Register\n2. Login\n\n0. exit");
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 0:
+                    Console.Clear();
+                    Console.Write("Are you sure you want to quit? (y/n): ");
+                    if (Console.ReadLine() == "y")
+                        System.Environment.Exit(1);
+                    break;
+                case 1:
+                    Login.Run();
+                    break;
+                case 2:
+                    Register.Run();
+                    break;
+                default: break;
+            }
 
-            Login.Run();
 
         }
 
