72b7239fa2f5a57b04c7fa8fe75568bc6e487014
sens1tiv <25586230+sens1tiv@users.noreply.github.com>
2019-11-15 15:08:27 +0100

    Update Welcome.cs

--- Snowbird/Final Test/Menus/Welcome.cs
+++ Snowbird/Final Test/Menus/Welcome.cs
@@ -11,17 +11,17 @@ namespace Final_Test.Menus {
                 Console.Write("\n\t\t\t\t\t\tWelcome to "); /**/ Snowbird.Write("Snowbird Wallet", ConsoleColor.Black, ConsoleColor.White);
                 Console.Write("!\n\n\t("); /**/ Snowbird.Write("1", ConsoleColor.Yellow); /**/ Console.Write(") "); Snowbird.WriteLine("Register", ConsoleColor.Green);
                 Console.Write("\t("); /**/ Snowbird.Write("2", ConsoleColor.Yellow); /**/ Console.Write(") "); Snowbird.WriteLine("Login", ConsoleColor.Blue);
-                Console.Write("\n\n\t("); /**/ Snowbird.Write("0", ConsoleColor.Yellow); /**/ Console.Write(") "); Snowbird.WriteLine("Quit", ConsoleColor.Red);
+                Console.Write("\n\n\t("); /**/ Snowbird.Write("Q", ConsoleColor.Yellow); /**/ Console.Write(") "); Snowbird.WriteLine("Quit", ConsoleColor.Red);
 
-                switch (int.Parse(Snowbird.KeyPressed()))
-                {
-                    case 0:
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                switch (input.Key) {
+                    case ConsoleKey.Q:
                         Snowbird.Exit();
                         break;
-                    case 1:
+                    case ConsoleKey.D1:
                         Register.Run();
                         break;
-                    case 2:
+                    case ConsoleKey.D2:
                         Login.Run();
                         isWelcome = false;
                         break;
