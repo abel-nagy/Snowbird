122b46058014127eb48f456c17053ad60e51a52d
sens1tiv <25586230+sens1tiv@users.noreply.github.com>
2019-11-15 11:36:27 +0100

    Added AddTransaction method WIP

--- Snowbird/Final Test/Menus/addWallets.cs
+++ Snowbird/Final Test/Menus/addWallets.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Final_Test.Menus
 {
-    public static class addWallets      // by Patrik
-    {
+    public static class addWallets {        // by Patrik
+
         public static void Run()
         {
             Console.Clear();
@@ -59,7 +55,7 @@ namespace Final_Test.Menus
             // Date
             DateTime myDateTime = DateTime.Now;
             string dateTime = myDateTime.ToString("yyyy-MM-dd HH:mm:ss");
-
+            /*
             Random r = new Random();
             if(type == 1)
             {
@@ -78,9 +74,47 @@ namespace Final_Test.Menus
             query = "INSERT INTO `wallets` (`id`, `user_id`, `type`, `amount`, `currency`, `account_name`, `account_number`, `description`, `created_at`) VALUES (NULL, '" + Login.GetUserId(Snowbird.user.Username) + "', '0', '" + amount + "', '" + currency + "', NULL, NULL, NULL, '" + dateTime + "');";
             Snowbird.db.NonQuery(query);
 
-
+            */
 
         }
 
+        public static void AddTransaction() {
+
+            bool isTrans = true;
+
+            while (isTrans) {
+
+                Console.Clear();
+                Console.Write("\n\t\t\t\t\t\tWelcome "); /**/ Snowbird.Write(Snowbird.user.Username, ConsoleColor.Blue);
+
+                Console.WriteLine("! \n\n\t- What type of transaction do you want to add?\n");
+
+                Console.Write("\t\t("); /**/ Snowbird.Write("1", ConsoleColor.Yellow); /**/ Console.Write(") "); /**/ Snowbird.WriteLine("Income", ConsoleColor.DarkGreen);
+                Console.Write("\t\t("); /**/ Snowbird.Write("2", ConsoleColor.Yellow); /**/ Console.Write(") "); /**/ Snowbird.WriteLine("Expense", ConsoleColor.DarkRed);
+                Console.Write("\t\t("); /**/ Snowbird.Write("3", ConsoleColor.Yellow); /**/ Console.Write(") "); /**/ Snowbird.Write("Transfer", ConsoleColor.DarkRed); /**/ Console.WriteLine(" (To another Bank Account or one of your wallets)");
+
+                int input = Convert.ToInt32( Console.ReadKey() );
+                bool isAdd = false;
+
+                switch (input) {
+                    case 48:
+                        isTrans = false;
+                        break;
+                    case 27:
+                        isTrans = false;
+                        break;
+                    default:
+                        if(input > 0 && input < 4)
+                            isAdd = true;
+                        break;
+                }
+
+                if(isAdd) {
+
+                }
+
+            }
+            
+        }
     }
 }
