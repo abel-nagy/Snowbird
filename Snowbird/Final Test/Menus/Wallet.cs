90f3ebb9105a9702dd12b8936eb653a07d51967e
sens1tiv <25586230+sens1tiv@users.noreply.github.com>
2019-11-16 11:42:06 +0100

    Update Wallet.cs

--- Snowbird/Final Test/Menus/Wallet.cs
+++ Snowbird/Final Test/Menus/Wallet.cs
@@ -1,85 +1,111 @@
 ﻿using System;
+using System.Globalization;
 using System.Text.RegularExpressions;
 
 namespace Final_Test.Menus
 {
     public static class Wallet {        // by Patrik and Ábel
 
-        public static void addWallet(int GetUserId)
-        {
-            Console.Clear();
-            Console.Write("\n\t\t\t\t\t\tWelcome "); /**/ Snowbird.Write(Snowbird.user.Username, ConsoleColor.Blue);
-            Console.WriteLine("\n\t\t\t\t\t\tCreate new wallet\n");
-            string query;
+        public static void AddWallet() {
 
-            // Type
-            int type = 0;
-            Console.WriteLine("Choose type: ");
-            Console.Write("  ("); /**/ Snowbird.Write("1", ConsoleColor.Yellow); /**/ Console.Write(") "); Snowbird.WriteLine("Wallet", ConsoleColor.Red, ConsoleColor.Black);
-            Console.Write("  ("); /**/ Snowbird.Write("2", ConsoleColor.Yellow); /**/ Console.Write(") "); Snowbird.WriteLine("Account", ConsoleColor.Red, ConsoleColor.Black);
-            switch (Snowbird.KeyPressed())
-            {
-                case "1":
-                    type = 0;
-                    break;
-                case "2":
-                    type = 1;
-                    break;
-                default:
-                    break;
+            bool runThis = true;
+            while(runThis) {
+
+                int type = 0;
+
+                bool runThis2 = true;
+                while(runThis2) {
+
+                    Console.Clear();
+
+                    Console.Write( "\n\t\t\t\t\t\tWelcome " ); /**/ Snowbird.Write( Snowbird.user.Username, ConsoleColor.Blue );
+                    Console.Write( "!\n\n\tCreate new wallet " ); /**/ Snowbird.WriteLine("1\n", ConsoleColor.DarkYellow);
+
+                        // Type
+                    Console.WriteLine( "\t- Choose type:\n" );
+
+                    Console.Write( "\t\t(" ); /**/ Snowbird.Write( "1", ConsoleColor.Yellow ); /**/ Console.Write( ") " ); Snowbird.WriteLine( "Wallet", ConsoleColor.Cyan);
+                    Console.Write( "\t\t(" ); /**/ Snowbird.Write( "2", ConsoleColor.Yellow ); /**/ Console.Write( ") " ); Snowbird.WriteLine( "Account", ConsoleColor.Magenta);
+
+                    switch(Snowbird.KeyPressed()) {
+                        case "1":
+                            type = 0;
+                            runThis2 = false;
+                            break;
+                        case "2":
+                            type = 1;
+                            runThis2 = false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+
+                runThis2 = true;
+                while(runThis2) {
+
+                    Console.Clear();
+
+                    Console.Write( "\n\t\t\t\t\t\tWelcome " ); /**/ Snowbird.Write( Snowbird.user.Username, ConsoleColor.Blue );
+                    Console.Write( "!\n\n\tCreate new wallet " ); /**/ Snowbird.WriteLine("2\n", ConsoleColor.DarkYellow);
+
+                        // Amount
+                    double amount = -1.0;
+                    Console.Write( "\n\tAmount: " );
+                    amount = double.Parse( Snowbird.GetNumbers(), CultureInfo.InvariantCulture.NumberFormat );
+
+                        // Currency
+                    string currency = "huf";
+
+                    Console.WriteLine( "\n\tCurrency:\n" );
+                    Console.Write( "\t(" ); /**/ Snowbird.Write( "1", ConsoleColor.Yellow ); /**/ Console.Write( ") " ); Snowbird.WriteLine( "HUF", ConsoleColor.Green );
+                    Console.Write( "\t(" ); /**/ Snowbird.Write( "2", ConsoleColor.Yellow ); /**/ Console.Write( ") " ); Snowbird.WriteLine( "EUR", ConsoleColor.Green );
+                    Console.Write( "\t(" ); /**/ Snowbird.Write( "3", ConsoleColor.Yellow ); /**/ Console.Write( ") " ); Snowbird.WriteLine( "USD", ConsoleColor.Green );
+
+                    switch(Snowbird.KeyPressed()) {
+                        case "1":
+                            currency = "huf";
+                            runThis2 = false;
+                            break;
+                        case "2":
+                            currency = "eur";
+                            runThis2 = false;
+                            break;
+                        case "3":
+                            currency = "usd";
+                            runThis2 = false;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    // Date
+                    DateTime myDateTime = DateTime.Now;
+                    string dateTime = myDateTime.ToString( "yyyy-MM-dd HH:mm:ss" );
+
+                    Random r = new Random();
+                    if(type == 1) {
+                        string account_name, account_number;
+                        int account_number_1, account_number_2;
+                        Console.Write( "Account Name: " );
+                        account_name = Console.ReadLine();
+
+                        // ezt nemtudom hogy kellene, megirom így egyenlőre
+                        account_number_1 = r.Next( 10000000, 100000000 );
+                        account_number_2 = r.Next( 10000000, 100000000 );
+                        account_number = "00000000" + account_number_1 + account_number_2;
+                        Snowbird.db.NonQuery( "INSERT INTO wallets (id,   user_id,                        type,           amount,           currency,           account_name,           account_number,           description, created_at) " +
+                                              "VALUES (             NULL, '" + Snowbird.user.UserId + "', '" + type + "', '" + amount + "', '" + currency + "', '" + account_name + "', '" + account_number + "', NULL,        '" + dateTime + "');" );
+
+                    }
+                    /*
+                    query = "INSERT INTO `wallets` (`id`, `user_id`, `type`, `amount`, `currency`, `account_name`, `account_number`, `description`, `created_at`) " +
+                            "VALUES (NULL, '" + Snowbird.user.UserId + "', '0', '" + amount + "', '" + currency + "', NULL, NULL, NULL, '" + dateTime + "');";
+                    Snowbird.db.NonQuery( query );*/
+
+                }
             }
-
-            float amount = 0;
-            
-
-            // Amount
-            Console.Write("Amount: ");
-            amount = Convert.ToSingle(Snowbird.GetNumbers());
-
-            // Currency
-            string currency = "huf";
-            Console.WriteLine("Currency:");
-            Console.Write("  ("); /**/ Snowbird.Write("1", ConsoleColor.Yellow); /**/ Console.Write(") "); Snowbird.WriteLine("HUF", ConsoleColor.Green, ConsoleColor.Gray);
-            Console.Write("  ("); /**/ Snowbird.Write("2", ConsoleColor.Yellow); /**/ Console.Write(") "); Snowbird.WriteLine("EUR", ConsoleColor.Green, ConsoleColor.Gray);
-            Console.Write("  ("); /**/ Snowbird.Write("3", ConsoleColor.Yellow); /**/ Console.Write(") "); Snowbird.WriteLine("USD", ConsoleColor.Green, ConsoleColor.Gray);
-            switch (Snowbird.KeyPressed())
-            {
-                case "1":
-                    currency = "huf";
-                    break;
-                case "2":
-                    currency = "eur";
-                    break;
-                case "3":
-                    currency = "usd";
-                    break;
-                default: break;
-            }
-
-            // Date
-            DateTime myDateTime = DateTime.Now;
-            string dateTime = myDateTime.ToString("yyyy-MM-dd HH:mm:ss");
-
-            Random r = new Random();
-            if(type == 1)
-            {
-                string account_name, account_number;
-                int account_number_1, account_number_2;
-                Console.Write("Account Name: ");
-                account_name = Console.ReadLine();
-
-                // ezt nemtudom hogy kellene, megirom így egyenlőre
-                account_number_1 = r.Next(0, 99999999);
-                account_number_2 = r.Next(0, 99999999);
-                account_number = "00000000" + account_number_1 + account_number_2;
-                query = "INSERT INTO `wallets` (`id`, `user_id`, `type`, `amount`, `currency`, `account_name`, `account_number`, `description`, `created_at`) VALUES (NULL, '" + GetUserId + "', '" + type + "', '" + amount + "', '" + currency + "', '" + account_name + "', '" + account_number + "', NULL, '" + dateTime + "');";
-                Snowbird.db.NonQuery(query);
-            }
-
-            query = "INSERT INTO `wallets` (`id`, `user_id`, `type`, `amount`, `currency`, `account_name`, `account_number`, `description`, `created_at`) VALUES (NULL, '" + GetUserId + "', '0', '" + amount + "', '" + currency + "', NULL, NULL, NULL, '" + dateTime + "');";
-            Snowbird.db.NonQuery(query);
-
-
         }
         
 
